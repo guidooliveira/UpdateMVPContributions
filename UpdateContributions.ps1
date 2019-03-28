d2632a7be4f7c675e8c473bcff5ed5a21c7d88c0
Guido Oliveira <guido.oliveira@live.com>
2019-03-27 22:59:19 -0300

    Update UpdateContributions.ps1

--- UpdateContributions.ps1
+++ UpdateContributions.ps1
@@ -1,55 +1,58 @@
-﻿#Install-module -Name MVP 
+#requires -Modules MVP
 
 $SubscriptionKey = 'aaabbbbcdbbcjdbjkcdjk8129310i0asd' 
-Set-MVPConfiguration -SubscriptionKey $SubscriptionKey
 $ContribuitionLimitGet = 200
 $StartCicleDate = "2018-04-01"
-
 $GoogleApiId = "sdklansdjhausfaksfnklasjansjvnajkscnjasjc"
 
+Set-MVPConfiguration -SubscriptionKey $SubscriptionKey
 #Function Get Views in Youtube Videos
-function GetViewsYoutube($videoId,$GoogleApiKey)
-{
-
-    $video_url = "https://www.googleapis.com/youtube/v3/videos?id=$videoId&key=$GoogleApiKey&part=snippet,contentDetails,statistics,status"
-    $web_client = new-object system.net.webclient
-    $build_info=$web_client.DownloadString("$($video_url)") | ConvertFrom-Json
-    return $build_info.items.statistics.viewCount
-
+function Get-YoutubeViewCount {
+    Param(
+        # Parameter help description
+        [Parameter(Mandatory = $true)]
+        [System.String]$GoogleApiKey,
+        [Parameter(Mandatory = $true)]
+        [System.String]$VideoId
+    )
+    PROCESS{
+        $RestCall = @{
+            Uri = 'https://www.googleapis.com/youtube/v3/videos?id={0}&key={1}&part=snippet,contentDetails,statistics,status' -f $VideoId, $GoogleApiKey
+        }
+        $Result = Invoke-RestMethod @RestCall
+    
+        Write-Output -InputObject $Result.items.statistics.viewCount
+    }
 }
 
 $Contributions = Get-MVPContribution -Limit $ContribuitionLimitGet
 
-foreach($contribs in $Contributions)
-{
-    if($contribs.ContributionTypeName -eq "Video/Webcast/Podcast")
-    {
+foreach ($contribs in $Contributions) {
+    if ($contribs.ContributionTypeName -eq "Video/Webcast/Podcast") {
         $contributionId = $contribs.ContributionId 
         $youtubeUrl = $contribs.ReferenceUrl 
-
        
-        if($contribs.ReferenceUrl.Contains('youtu'))
-        {
-            $videoId = $youtubeUrl.Replace('https://youtu.be/','').Replace('https://www.youtube.com/watch?v=','')
+        if ($contribs.ReferenceUrl.Contains('youtu')) {
+            $videoId = $youtubeUrl.Replace('https://youtu.be/', '').Replace('https://www.youtube.com/watch?v=', '')
             $ContribDate = $contribs.StartDate           
             
-            if([datetime]$ContribDate -ge [datetime]$StartCicleDate)
-            {
-                    
-                    $views = GetViewsYoutube $videoId $GoogleApiId
-                    Set-MVPContribution -ContributionID $contributionId -AnnualReach $views
-                    Write-Host "Updating Contribution: $($contributionId) - AnnualReach: $($views) - $($ContribDate)"
-                    #Alert - Sometimes the MVP API returns error 500.
-                    Start-Sleep -s 15
-            }
-            else
-            {
-                    Write-Warning "Date Out Of Range - Contribution: $($contributionId) - Not Updated - $($ContribDate)"
-            }
-           
-            
-        }
+            if ([datetime]$ContribDate -ge [datetime]$StartCicleDate) {
 
+                $views = Get-YoutubeViewCount -GoogleApiKey $GoogleApiKey -VideoId $VideoId
+                
+                try{
+                    Set-MVPContribution -ContributionID $contributionId -AnnualReach $views -ErrorAction 'Stop'
+                }
+                catch{
+                    Write-Error -Message $Error[0].exception.Message
+                }
+                Write-Host "Updating Contribution: $($contributionId) - AnnualReach: $($views) - $($ContribDate)"
+                #Alert - Sometimes the MVP API returns error 500.
+                Start-Sleep -s 15
+            }
+            else {
+                Write-Warning "Date Out Of Range - Contribution: $($contributionId) - Not Updated - $($ContribDate)"
+            }   
+        }
     }
 }
-
